92dc41edd7061c0826631fa6c803fbf0236e95e5
dr0te <andrybowshik@gmail.com>
2026-07-07 00:29:44 +0300

    first commit

---
+++ archive-site/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return(
+        <header>
+            <h1>dr0te site</h1>
+
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Collection</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
