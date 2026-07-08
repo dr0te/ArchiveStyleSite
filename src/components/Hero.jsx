9f877dfd6654d8680918dbcaf1a48e1eefb63901
dr0te <andrybowshik@gmail.com>
2026-07-08 19:52:21 +0300

    Added Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+function Hero() {
+    return (
+        <>
+        <h2> This is a place for your vibe photos.</h2>
+
+         <p>
+            Upload your photos, share them with others. and let people like and save them.
+        </p>
+        </>
+    );
+}
+
+export default Hero;
