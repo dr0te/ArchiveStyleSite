bc64ce95d74237d4368a4f497a3ac23603a0ca62
dr0te <andrybowshik@gmail.com>
2026-07-08 21:52:51 +0300

    Added Gallery and changed theme to light colors

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,72 @@
+function Gallery() {
+    return(
+        <>
+        <section className="gallery">
+        <div className="photo-card">
+            <div className="card-header">
+                <img className="avatar" src="/avatar.png"  alt="Avatar" />
+                <h3>Username</h3>
+            </div>
+
+            <img className="photo" src="/photo.jpg" alt="Photo" />
+
+            <div className="card-action">
+                <button>Like</button>
+                <button>Save</button>
+            </div>
+            <p>Description</p>
+        </div>
+        </section>
+         <section className="gallery">
+        <div className="photo-card">
+            <div className="card-header">
+                <img className="avatar" src="/avatar.png"  alt="Avatar" />
+                <h3>Username</h3>
+            </div>
+
+            <img className="photo" src="/photo.jpg" alt="Photo" />
+
+            <div className="card-action">
+                <button>Like</button>
+                <button>Save</button>
+            </div>
+            <p>Description</p>
+        </div>
+        </section>
+         <section className="gallery">
+        <div className="photo-card">
+            <div className="card-header">
+                <img className="avatar" src="/avatar.png"  alt="Avatar" />
+                <h3>Username</h3>
+            </div>
+
+            <img className="photo" src="/photo.jpg" alt="Photo" />
+
+            <div className="card-action">
+                <button>Like</button>
+                <button>Save</button>
+            </div>
+            <p>Description</p>
+        </div>
+        </section>
+         <section className="gallery">
+        <div className="photo-card">
+            <div className="card-header">
+                <img className="avatar" src="/avatar.png"  alt="Avatar" />
+                <h3>Username</h3>
+            </div>
+
+            <img className="photo" src="/photo.jpg" alt="Photo" />
+
+            <div className="card-action">
+                <button>Like</button>
+                <button>Save</button>
+            </div>
+            <p>Description</p>
+        </div>
+        </section>
+        </>
+    )
+}
+
+export default Gallery
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,9 @@ function Header() {
             <h1>Archive Style Gram</h1>
 
             <nav>
+                <a href="#">Search</a>
                 <a href="#">Home</a>
-                <a href="#">Collection</a>
-                <a href="#">About</a>
-                <a href="#">Contact</a>
+                <a href="#">Publish</a>
             </nav>
         </header>
     );
